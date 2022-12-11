cc4076a9fd80a7166f3ba12a732a82a2107cb902
Thiago Buarque <thiago.buarqque@gmail.com>
2022-12-10 21:36:16 -0300

    Analytics - Add JSONSerializer tests

--- analytics/src/test/java/com/evry/analytics/AnalyticsApplicationTests.java
+++ analytics/src/test/java/com/evry/analytics/AnalyticsApplicationTests.java
@@ -1,13 +1,50 @@
 package com.evry.analytics;
 
+import com.evry.analytics.DTO.UserDTO;
+import com.evry.analytics.common.JSONSerializer;
+import com.evry.analytics.common.exceptions.JSONSerializableException;
+import com.evry.analytics.entity.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AnalyticsApplicationTests {
 
+	final JSONSerializer jsonSerializer = new JSONSerializer();
+
 	@Test
-	void contextLoads() {
+	void testJSONSerializationExceptionThrowing() {
+		User user = new User();
+		user.setName("James Matthew");
+		user.setUserId(1L);
+
+		UserDTO userDTO = new UserDTO(user);
+
+		Assertions.assertDoesNotThrow(() -> {
+			jsonSerializer.convertToJSONString(userDTO);
+		});
+
+		Assertions.assertThrows(JSONSerializableException.class, () -> {
+			jsonSerializer.convertToJSONString(user);
+		});
 	}
 
+	@Test
+	void testJSONSerialization() {
+		User user = new User();
+		user.setName("James Matthew");
+		user.setUserId(1L);
+
+		UserDTO userDTO = new UserDTO(user);
+		;
+
+		String expected = "\"name\":\"James Matthew\",\"userId\":1";
+		try {
+			Assertions.assertEquals(expected,
+				jsonSerializer.convertToJSONString(userDTO)
+			);
+		} catch (JSONSerializableException | IllegalAccessException exception) {
+		}
+	}
 }
