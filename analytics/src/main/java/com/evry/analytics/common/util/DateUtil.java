f476424ba7eede02b760215cdf025ef77f8c00e8
Thiago Buarque <thiago.buarque@liferay.com>
2023-01-17 16:25:36 -0300

    Analytics - Remove unnecessary methods

--- analytics/src/main/java/com/evry/analytics/common/util/DateUtil.java
+++ analytics/src/main/java/com/evry/analytics/common/util/DateUtil.java
@@ -21,14 +21,6 @@ public class DateUtil {
         return offsetDateTime.withNano(999999999);
     }
 
-    public static ZonedDateTime getAtDayMax(ZonedDateTime zonedDateTime) {
-        zonedDateTime = zonedDateTime.withHour(23);
-        zonedDateTime = zonedDateTime.withMinute(59);
-        zonedDateTime = zonedDateTime.withSecond(59);
-
-        return zonedDateTime.withNano(999999999);
-    }
-
     public static LocalDateTime getAtDayMin(LocalDateTime localDateTime) {
         localDateTime = localDateTime.withHour(0);
         localDateTime = localDateTime.withMinute(0);
@@ -43,13 +35,5 @@ public class DateUtil {
         offsetDateTime = offsetDateTime.withSecond(0);
 
         return offsetDateTime.withNano(0);
-    }
-
-    public static ZonedDateTime getAtDayMin(ZonedDateTime zonedDateTime) {
-        zonedDateTime = zonedDateTime.withHour(0);
-        zonedDateTime = zonedDateTime.withMinute(0);
-        zonedDateTime = zonedDateTime.withSecond(0);
-
-        return zonedDateTime.withNano(0);
     }
 }
