b07b6a089456274395b7a9dcab1f6cae965d39e5
Thiago Buarque <thiago.buarque@liferay.com>
2023-03-22 17:21:24 -0300

    Analytics - SF

--- analytics/src/main/java/com/evry/analytics/bigquery/SchemaCreator.java
+++ analytics/src/main/java/com/evry/analytics/bigquery/SchemaCreator.java
@@ -1,7 +1,7 @@
-package com.evry.analytics.config;
+package com.evry.analytics.bigquery;
 
 import com.google.cloud.bigquery.BigQuery;
-import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.BigQueryException;
 import com.google.cloud.bigquery.DatasetInfo;
 import com.google.cloud.bigquery.Field;
 import com.google.cloud.bigquery.Schema;
@@ -15,40 +15,62 @@ import com.google.cloud.bigquery.testing.RemoteBigQueryHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+// This class is just for learning purposes. I'll not keep updating it, since
+// it costs money to use BigQuery.
 @Component
 public class SchemaCreator {
 
     @Autowired
     public SchemaCreator() {
-        this.bigQuery = BigQueryOptions.getDefaultInstance().getService();
-        this.createTestSchema();
+        RemoteBigQueryHelper bigqueryHelper = RemoteBigQueryHelper.create();
+
+        this.bigQuery = bigqueryHelper.getOptions().getService();
+
+        this.createSchema();
+        this.createTables();
     }
 
-    void createTestSchema() {
-        System.out.println("Creating schema");
-        RemoteBigQueryHelper bigqueryHelper = RemoteBigQueryHelper.create();
-        BigQuery bigquery = bigqueryHelper.getOptions().getService();
-
+    void createSchema() {
         String datasetId = "workspace2";
 
         DatasetInfo dataset =
                 DatasetInfo.newBuilder(datasetId)
                         .setDescription(
-                                "Esta é uma tabela criada através do " + "client BQ para java")
+                                "This is dataset crated using BQ API.")
                         .setFriendlyName("dataset_friendly_name")
                         .build();
 
+        System.out.printf("Creating schema '%s'.", datasetId);
+
+        try {
+            bigQuery.create(dataset);
+        } catch (BigQueryException bigQueryException) {
+            System.out.printf("Cannot create schema '%s'.", datasetId);
+        }
+    }
+
+    private void createTables() {
         Schema schema =
                 Schema.of(
                         Field.of("stringField", StandardSQLTypeName.STRING),
                         Field.of("booleanField", StandardSQLTypeName.BOOL));
 
         TableDefinition tableDefinition = StandardTableDefinition.of(schema);
-        TableInfo tableInfo =
-                TableInfo.newBuilder(TableId.of(datasetId, "table1"), tableDefinition).build();
 
-        bigquery.create(dataset);
-        bigQuery.create(tableInfo);
+        String schemaName = "workspace2";
+
+        String tableName = "table1";
+
+        TableInfo tableInfo = TableInfo.newBuilder(
+                TableId.of(schemaName, tableName), tableDefinition
+        ).build();
+
+        try {
+            bigQuery.create(tableInfo);
+        } catch (BigQueryException bigQueryException) {
+            System.out.printf("Cannot create table '%s' for schema '%s'",
+                              tableName, schemaName);
+        }
     }
 
     private final BigQuery bigQuery;
